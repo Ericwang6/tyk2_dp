d9d0566f94932136ee5ee97f45e89dd5d15c860f
Ericwang6 <ericwangyz602@gmail.com>
2021-05-03 23:48:00 +0800

    tyk2 test cases

---
+++ prepare_gaussian_input.py
@@ -0,0 +1,41 @@
+import dpdata
+import numpy as np
+import os
+
+
+def make_gaussian_input(system,
+                        frame_idx=0, 
+                        fname=None, 
+                        header='#force wB97XD/6-31G(d)',
+                        foot="",
+                        title='mol', 
+                        charge=0, 
+                        mult=1):
+    ret = header + "\n\n" + title + "\n\n" + str(charge) + " " + str(mult) + "\n"
+    coord = system.data["coords"][frame_idx].reshape(-1, 3)
+    for atype, c in zip(system.data["atom_types"], coord):
+        ret += system.data["atom_names"][atype] + " "
+        ret += " ".join([str(x) for x in c])
+        ret += "\n"
+    if foot:
+        ret += "\n" + foot + "\n"
+    else:
+        ret += "\n"
+    if fname:
+        if not os.path.exists(os.path.dirname(fname)):
+            os.mkdir(os.path.dirname(fname))
+        with open(fname, "w+") as f:
+            f.write(ret)
+    return ret
+
+
+if __name__ == "__main__":
+    ligs = np.loadtxt("SYSTEMS.txt", dtype=np.str)
+    for lig in ligs:
+        gro_file = f"ligands_5ns/{lig}/md_traj.gro"
+        system = dpdata.System(gro_file, fmt='gromacs/gro', type_map=['H','C','N','O','Cl'])
+        # the first 1ns traj
+        for frame_idx in range(system.get_nframes() // 5):
+            make_gaussian_input(system,
+                                fname=f"gaussian/{lig}/{frame_idx}.gjf",
+                                header="%nproc=32\n%mem=40GB\n#force wB97XD/6-31G(d)")
---
+++ run.py
@@ -0,0 +1,33 @@
+import os, glob
+import numpy as np
+
+
+def write_pbs(gjfs, gjf_dir=None, batch=20, cache_prefix="cache", submit=False,
+              env="~/gaussian/g09.bashrc"):
+    if not gjf_dir:
+        gjf_dir = os.path.dirname(gjfs[0])
+    wdir = os.path.join(gjf_dir, cache_prefix)
+    if not os.path.exists(wdir):
+        os.mkdir(wdir)
+    cnt = 0
+    for ii in range(0, len(gjfs), batch):
+        with open(os.path.join(wdir, f'{cnt}.pbs'), "w+") as f:
+            f.write("#!/bin/bash\n#PBS -N gaussian\n#PBS -q C_32_64\n#PBS -l select=1:ncpus=32\n")
+            f.write(f"source {env}\n")
+            f.write("cd $PBS_O_WORKDIR\n")
+            f.write(f"cd {os.path.relpath(gjf_dir, wdir)}\n")
+            for gjf in gjfs[ii: ii+batch]:
+                f.write(f"g16 {os.path.basename(gjf)}\n")
+        # submit
+        if submit:
+            os.system(f"cd {wdir} && qsub {cnt}.pbs")
+        cnt += 1
+
+if __name__ == "__main__":
+    ligs = np.loadtxt("SYSTEMS.txt", dtype=np.str)
+    for lig in ligs:
+        gjf_dir = f"gaussian/{lig}"
+        gjfs = list(glob.glob(os.path.join(gjf_dir, "*gjf")))
+        write_pbs(gjfs, gjf_dir, batch=1000)
+
+    
